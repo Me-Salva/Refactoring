2bd7483019ac427bf9fcb1d1eefa84e2d49dc434
Guilherme Diogo <up201806340@fe.up.pt>
2021-12-20 20:46:18 +0000

    Example 1

--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -8,4 +8,8 @@ public class OrderLine {
         this.product = product;
         this.quantity = quantity;
     }
+
+    public double getOrderLineTotal(){
+        return product.getPrice()*quantity;
+    }
 }
